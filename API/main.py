0f6bf9c0c73c6bd5142d199af42b75bdb4fc5e45
ArnoChuang <j18970678@gmail.com>
2025-02-17 10:28:55 +0800

    set cros mode

--- API/main.py
+++ API/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from model import pred_inputs
 from predict import predict
 
 app = FastAPI()
+
+# ➜ 允許所有來源（讓所有人都能呼叫 API）
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict/")
 async def pred(pred_inputs:pred_inputs):
